3ba0ace3007a0edf3925e325170f3ff54ca1eaaf
Eduardo Robles Elvira <edulix@wadobo.com>
2014-03-06 23:17:17 +0100

    fixing bootstrap script

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-apt-get install update
-apt-get install aptitude
+apt-get update
 
 wget -qO /tmp/puppetlabs-release-wheezy.deb https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
 
 dpkg -i /tmp/puppetlabs-release-wheezy.deb
 rm /tmp/puppetlabs-release-wheezy.deb
 
-apt-get update
-apt-get install -y puppet
+apt-get-update
+apt-get install -y puppet aptitude
+
+echo "saving path to manifests/init.pp so that backup script knows.."
+SCRIPT_PATH=$(readlink -f "$0")
+INIT_PATH=$(dirname $SCRIPT_PATH)/../manifests/init.pp
+INIT_PATH=$(realpath $INIT_PATH)
+
+echo "$INIT_PATH" > /root/.eo_puppet_manifests_path
 
 echo "Puppet installed!"
