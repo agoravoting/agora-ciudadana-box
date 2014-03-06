45a004710e8b8f3e445876ade0f48bcf3ac49801
Eduardo Robles Elvira <edulix@wadobo.com>
2014-03-06 23:18:14 +0100

    fixing typo

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -7,7 +7,7 @@ wget -qO /tmp/puppetlabs-release-wheezy.deb https://apt.puppetlabs.com/puppetlab
 dpkg -i /tmp/puppetlabs-release-wheezy.deb
 rm /tmp/puppetlabs-release-wheezy.deb
 
-apt-get-update
+apt-get update
 apt-get install -y puppet aptitude
 
 echo "saving path to manifests/init.pp so that backup script knows.."
